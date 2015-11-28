0f0174ec52e4f75023f6c1e90bf8a840c3582d16
F10 <mr.greyforever@gmail.com>
2015-11-28 15:50:28 +0800

    before debuging intercode generation 	new file:   include/OP.h 	modified:   include/Expr.h 	modified:   include/Node.h 	modified:   include/PAF.h 	modified:   include/Quadruple.h 	modified:   include/Stmt.h 	modified:   inter/Expr.cpp 	modified:   inter/Stmt.cpp 	modified:   inter/instructions 	modified:   parser/parsedeclpaf.cpp 	modified:   parser/parsestmt.cpp 	modified:   symboltable/PAF.cpp 	modified:   tags 	modified:   test/test_parser.cpp

---
+++ include/OP.h
@@ -0,0 +1,22 @@
+#include <string>
+#ifndef OP_H
+#define OP_H
+enum OP {
+	I_READ = 0 ,
+	I_WRITE  ,
+	I_IF  ,
+	I_IFFALSE ,
+	I_GOTO,
+	I_PARAM ,
+	I_CALLPROC ,
+	I_CALLFUNC ,
+	I_COPY ,
+	I_COPYIND ,
+	I_INDCOPY  ,
+	I_MULT ,
+	I_DIV ,
+	I_ADD ,
+	I_MINUS ,
+};
+std::string *OptoString;
+#endif
